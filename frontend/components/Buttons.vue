6dbbf52b7dbe6c2347c120b2183c333a1bf16320
Yannick <yannick.gardiol@gmail.com>
2021-12-01 17:55:31 +0100

    Remove categories

--- frontend/components/Buttons.vue
+++ frontend/components/Buttons.vue
@@ -1,35 +1,9 @@
 <template>
   <div class="">
-    <div v-if="error">
-      {{ error }}
-    </div>
-    <div v-else>
-      <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8">
-        <nuxt-link
-          v-for="category in categories"
-          :to="`/categories/${category.slug}`"
-          :key="category.id"
-          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow"
-        >
-          {{ category.name }}
-        </nuxt-link>
-      </div>
-    </div>
     <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8">
-      <nuxt-link
-        to="/api-test"
-        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow"
-      >
-        API samples
-      </nuxt-link>
+      <MyButton to="/"> Home </MyButton>
+      <MyButton to="/api-test"> API samples </MyButton>
     </div>
+    <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8"></div>
   </div>
 </template>
-
-<script setup>
-const config = useRuntimeConfig();
-const { data, error } = await useAsyncData("categories", () =>
-  fetch(`${config.STORE_URL}/categories`).then((response) => response.json())
-);
-const categories = data;
-</script>
