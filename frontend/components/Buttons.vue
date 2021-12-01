cc734851c2cf7c690c6ad48bb1ed16e3f2f17e99
Yannick <yannick.gardiol@gmail.com>
2021-12-01 18:05:37 +0100

    Remove categories

--- frontend/components/Buttons.vue
+++ frontend/components/Buttons.vue
@@ -4,6 +4,5 @@
       <MyButton to="/"> Home </MyButton>
       <MyButton to="/api-test"> API samples </MyButton>
     </div>
-    <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8"></div>
   </div>
 </template>
